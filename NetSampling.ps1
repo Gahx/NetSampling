6599cf5bd4bb9baf681d79b228604b254a4a8ee9
Gahx <52219008+Gahx@users.noreply.github.com>
2022-04-14 09:27:40 -0400

    Update NetSampling.ps1

--- NetSampling.ps1
+++ NetSampling.ps1
@@ -1,11 +1,11 @@
 ﻿Function NetSampling() 
 {
-    Ping 192.168.0.1 -n 5
-    Test-Connection -ComputerName Lappy, GahxPC  #Add more devices if desired
+    Ping (router/switch ip) -n 5
+    Test-Connection -ComputerName (Computer Name(s)  #Add more devices if desired
     Sleep 300
     ipconfig /all
     Sleep 300
-    Test-NetConnection -TraceRoute www.google.com
+    Test-NetConnection -TraceRoute www.msn.com
       
 }
 
