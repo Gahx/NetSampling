83fe7696c78b32076a2208a24144125c18ac7ea9
Gahx <52219008+Gahx@users.noreply.github.com>
2022-04-14 09:24:44 -0400

    Add files via upload

---
+++ NetSampling.ps1
@@ -0,0 +1,13 @@
+﻿Function NetSampling() 
+{
+    Ping 192.168.0.1 -n 5
+    Test-Connection -ComputerName Lappy, GahxPC  #Add more devices if desired
+    Sleep 300
+    ipconfig /all
+    Sleep 300
+    Test-NetConnection -TraceRoute www.google.com
+      
+}
+
+
+    NetSampling
