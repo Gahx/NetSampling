633ec2627532d969cfd4701f7b7fa926442a7ecc
Gahx <52219008+Gahx@users.noreply.github.com>
2022-04-25 15:19:40 -0400

    Update NetSampling.ps1

--- NetSampling.ps1
+++ NetSampling.ps1
@@ -3,9 +3,9 @@
     $Name = Read-Host -Prompt 'What computer would you like to test'
     Ping (router/switch ip) -n 5
     Test-Connection -ComputerName $Name  
-    Sleep 300
+    Sleep 20
     ipconfig /all
-    Sleep 300
+    Sleep 20
     Test-NetConnection -TraceRoute www.msn.com
       
 }
